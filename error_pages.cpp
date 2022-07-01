3343ff5a8c1b66ca481a72a3f02f0dce55b80b33
Kacper Pucylo <pucylek@wp.pl>
2022-07-01 14:42:06 +0200

    teapot

--- error_pages.cpp
+++ error_pages.cpp
@@ -7,9 +7,9 @@ const std::string generate_html(const std::string &key)
 	std::string html;
 	std::map<std::string, std::string> errors = http_table();
 	std::stringstream ss;
-	ss << "<html>\r\n<head><title>" << key << " " << errors[key] << "</title></head>\r\n";
-	ss << "<body>\r\n<center><h1>" << key << " " << errors[key] << "</h1></center>\r\n";
-	ss << "<hr><center>webserv</center>\r\n</body>\r\n</html>\r\n";
+	ss << "<html>\r\n<head><title><style>h1 {text-align: center;}p {text-align: center;}div {text-align: center;}</style>" << key << " " << errors[key] << "</title></head>\r\n";
+	ss << "<body>\r\n<h1>" << key << " " << errors[key] << "</h1>\r\n";
+	ss << "<hr>webserv\r\n</body>\r\n</html>\r\n";
 	std::cout << ss.str() << std::endl;
 	return (ss.str());
 }
