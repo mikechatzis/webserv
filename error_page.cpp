3d124f135208624a3153fc5c98d353250dd84cb7
Kacper Pucylo <pucylek@wp.pl>
2022-07-02 13:58:43 +0200

    error images

--- error_page.cpp
+++ error_page.cpp
@@ -8,7 +8,9 @@ std::string generate_html(const std::string &key)
 	std::stringstream ss;
 	ss << "<html>\r\n<head><title>h1 {text-align: center;}p {text-align: center;}div {text-align: center;}" << key << " " << errors[key] << "</title></head>\r\n";
 	ss << "<body>\r\n<h1>" << key << " " << errors[key] << "</h1>\r\n";
-	ss << "<hr>webserv\r\n</body>\r\n</html>\r\n";
+	ss << "<img style=\"margin-left: auto;\", src=\"https://http.cat/";
+	ss << key;
+	ss << "\">\r\n</body>\r\n</html>\r\n";
 	std::cout << ss.str() << std::endl;
 	return (ss.str());
 }
