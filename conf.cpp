64e114d9b9a82cbdf4760c359e5f1623ad3ffaef
Michail Chatzipanagiotou <mchatzip@c2s9d33.42wolfsburg.de>
2022-07-06 14:30:14 +0200

    added allowed_methods rule and a text coloring class

--- conf.cpp
+++ conf.cpp
@@ -9,9 +9,8 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 		bool port;
 		bool root;
 		bool host;
+		bool methods;
 	} doubles;
-
-	doubles.port = doubles.root = doubles.host = false;
 
 	std::ifstream f(file, std::ifstream::in);
 	if (f.fail())
@@ -30,7 +29,7 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 		for (size_t i = 0; cont[i]; i++)
 		{
 			if (cont[i] == '{')
-				while (cont[i] !='}')
+				while (cont[i] !='}' && cont[i])
 					i++;
 			if (!cont[i])
 				throw std::invalid_argument("no matching '}'");
@@ -38,28 +37,29 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 	}
 
 	while (!f.eof()){
-		char line[1000];
-		f.getline(line, 1000, '{');
+		char line[10000];
+		f.getline(line, 10000, '{');
 		std::string l_ine(line);
 		
 		l_ine.erase(std::remove_if(l_ine.begin(), l_ine.end(), isspace), l_ine.end());
 
 		/* CASE "SERVER" */
 		if (l_ine == "server"){
+			doubles.port = doubles.root = doubles.host = doubles.methods = false;
 			co->push_back(new conf_data());
 
 			std::vector<conf_data*>::iterator it= --co->end();
-			f.getline(line, 1000, '}');
+			f.getline(line, 10000, '}');
 			std::stringstream content(line);
-			std::string const rules[6] = {"server_names", "port", "error_page", "root", "host", ""};
+			std::string const rules[7] = {"server_names", "port", "error_page", "root", "host", "allowed_methods", ""};
 
 			while (!content.eof())
 			{
-				content.getline(line, 1000, ':');
+				content.getline(line, 10000, ':');
 				std::string li_ne(line);
 				li_ne.erase(remove_if(li_ne.begin(), li_ne.end(), isspace), li_ne.end());
 				int pos = 0;
-				for (size_t i = 0; i < 6; ++i)
+				for (size_t i = 0; i < 7; ++i)
 				{
 					if (li_ne == rules[i]){
 						pos = i + 1;
@@ -69,7 +69,7 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 				switch (pos)
 				{
 					case 1:
-						content.getline(line, 1000, ';');
+						content.getline(line, 10000, ';');
 						li_ne = line;
 						if (content.eof())
 								throw std::invalid_argument("no ';' found");
@@ -77,7 +77,7 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 						break;
 					
 					case 2:
-						content.getline(line, 1000, ';');
+						content.getline(line, 10000, ';');
 						if (doubles.port)
 							break;
 						else
@@ -94,7 +94,7 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 						}
 						break;
 					case 3:
-						content.getline(line, 1000, ';');
+						content.getline(line, 10000, ';');
 						{
 							if (content.eof())
 									throw std::invalid_argument("no ';' found");
@@ -119,7 +119,7 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 						}
 						break;
 					case 4:
-						content.getline(line, 1000, ';');
+						content.getline(line, 10000, ';');
 						if (doubles.root)
 							break;
 						else
@@ -130,7 +130,7 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 						(*it)->setRoot(line);
 						break;
 					case 5:
-						content.getline(line, 1000, ';');
+						content.getline(line, 10000, ';');
 						if (doubles.host)
 							break;
 						else
@@ -141,6 +141,30 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 						(*it)->setHost(line);
 						break;
 					case 6:
+						{
+							content.getline(line, 10000, ';');
+							if (doubles.methods)
+								break;
+							else
+								doubles.methods = true;
+							std::string const methods[3] = {"GET", "POST", "DELETE"};
+							if (content.eof())
+									throw std::invalid_argument("no ';' found");
+							li_ne = line;
+							char *token = strtok(line, " \n\t");
+							size_t j = 0;
+							for (size_t i = 0; token; ++i){
+								for (j = 0; j < 3; j++)
+									if (token == methods[j])
+										break;
+								token = strtok(NULL, " \n\t");
+							}
+							if (j == 3)
+								throw std::invalid_argument("Invalid HTTP method");
+							(*it)->setMethods(li_ne);
+						}
+						break;
+					case 7:
 						break;
 					default:
 						throw std::invalid_argument("invalid rule");
@@ -151,13 +175,13 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 		}
 		/* CASE "ERROR_PAGE" */
 		else if(l_ine == "error_page"){
-			f.getline(line, 1000, '}');
+			f.getline(line, 10000, '}');
 			std::stringstream content(line);
 			std::string const rules[2] = {"codes_to_filepath", ""};
 
 			while (!content.eof())
 			{
-				content.getline(line, 1000, ':');
+				content.getline(line, 10000, ':');
 				std::string li_ne(line);
 				li_ne.erase(remove_if(li_ne.begin(), li_ne.end(), isspace), li_ne.end());
 				int pos = 0;
@@ -171,7 +195,7 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 				switch (pos)
 				{
 					case 1:
-						content.getline(line, 1000, ';');
+						content.getline(line, 10000, ';');
 						{
 							if (content.eof())
 									throw std::invalid_argument("no ';' found");
@@ -225,34 +249,39 @@ int main(int ac, char **av){
 	}
 	catch(const std::exception& e)
 	{
-		Color::Modifier b_red(Color::BG_RED);
-		Color::Modifier f_black(Color::FG_BLACK);
-		Color::Modifier b_def(Color::BG_DEFAULT);
-		Color::Modifier f_def(Color::FG_DEFAULT);
-		std::cerr << b_red << f_black << "\nError: " << e.what() << b_def << '\n';
-		std::cerr << b_red << "Initiating with default settings" << b_def << f_def <<"\n\n" ;
+		Color::Modifier f_red(Color::Red);
+		Color::Modifier f_white(Color::White);
+		std::cerr << f_red << "\nError: " << e.what() << '\n';
+		std::cerr << "Initiating with default settings" << f_white <<"\n\n" ;
 		co = readConfFile(gconf);
 	}
 	
-	Color::Modifier f_green(Color::FG_GREEN);
-	Color::Modifier f_yellow(Color::FG_YELLOW);
-	Color::Modifier f_def(Color::FG_DEFAULT);
+	Color::Modifier f_green(Color::SeaGreen2);
+	Color::Modifier f_magenta(Color::Magenta2);
+	Color::Modifier f_yellow(Color::Yellow2);
+	Color::Modifier f_grey_b(Color::Grey93, 1);
+	Color::Modifier f_grey_lighter(Color::Grey100);
+	Color::Modifier reset(Color::White, 0, 1);
 	std::vector<conf_data*>::iterator it = co->begin();
 	for (size_t i = 0; i < co->size(); i++)
 	{
-		std::cout << "ELEMENT: " << i + 1 << f_green << "\n\nserver_names: " << (*it)->s_names() << "\nport: " << (*it)->s_port() <<
-		"\nroot_dir: " << (*it)->s_root() << "\nhost: " << (*it)->s_host() << std::endl << std::endl;
-		std::cout << f_def << "elements's error codes\n\n" << f_yellow ;
+		std::cout << f_grey_b << "\nELEMENT: " << i + 1 << " {" << reset
+		<< f_magenta << "\n\n\tserver_names: " << f_green << (*it)->s_names() 
+		<< f_magenta << "\n\tport: " << f_green << (*it)->s_port()
+		<< f_magenta << "\n\troot_dir: " << f_green << (*it)->s_root()
+		<< f_magenta << "\n\tallowed methods: " << f_green << (*it)->s_methods()
+		<< f_magenta << "\n\thost: " << f_green << (*it)->s_host() << std::endl << std::endl;
 		(*it)->printErrorCodes();
-		std::cout << f_def << std::endl << std::endl;
+		std::cout << f_grey_b << "}" << std::endl << std::endl;
 		++it;
 	}
 	
-	Color::Modifier f_blue(Color::FG_BLUE);
-	std::cout << "DEFAULT ERROR CODES\n\n";
+	Color::Modifier f_blue(Color::DeepSkyBlue2);
+	std::cout << f_grey_b << "DEFAULT ERROR CODES\n\n" << reset;
 	for (std::map<size_t, std::string>::iterator i = gconf->error_pages->begin(); i != gconf->error_pages->end(); i++)
 	{
-		std::cout << f_blue << "default_error_code: " << i->first << " path: " << i->second << f_def << std::endl;
+		std::cout << f_blue << "default_error_code: " << reset << i->first 
+				  << f_blue << " path: " << reset << i->second << std::endl;
 	}
 	
 	for (std::vector<conf_data*>::iterator ite = co->begin(); ite != co->end(); ++ite)
