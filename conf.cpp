27082b39c0fa7c672aeaee4367599bb5cb2ef36e
Michail Chatzipanagiotou <mchatzip@c2s9d33.42wolfsburg.de>
2022-07-06 10:36:40 +0200

    added multiple rules of the same name functionality, and additional data

--- conf.cpp
+++ conf.cpp
@@ -5,6 +5,14 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 		throw std::invalid_argument("configuration file: invalid name");
 	std::vector<conf_data*> *co = new std::vector<conf_data*>();
 
+	struct {
+		bool port;
+		bool root;
+		bool host;
+	} doubles;
+
+	doubles.port = doubles.root = doubles.host = false;
+
 	std::ifstream f(file, std::ifstream::in);
 	if (f.fail())
 		throw std::runtime_error("configuration file not found or corrupt");
@@ -14,22 +22,24 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 		std::ifstream c(file, std::ifstream::in);
 		if (c.fail())
 			throw std::runtime_error("configuration file not found or corrupt");
-		char cont[10000];
-		bzero(cont, 10000);
-		c.read(cont, 10000);
+		char cont[100000];
+		bzero(cont, 100000);
+		c.read(cont, 100000);
+		if (!strchr(cont, '{'))
+			throw std::invalid_argument("missing token '{'");
 		for (size_t i = 0; cont[i]; i++)
 		{
 			if (cont[i] == '{')
-				while (cont[i] !='}' && cont[i])
+				while (cont[i] !='}')
 					i++;
 			if (!cont[i])
-				throw std::invalid_argument("no matching curly brackets");
+				throw std::invalid_argument("no matching '}'");
 		}
 	}
 
 	while (!f.eof()){
-		char line[100];
-		f.getline(line, 100, '{');
+		char line[1000];
+		f.getline(line, 1000, '{');
 		std::string l_ine(line);
 		
 		l_ine.erase(std::remove_if(l_ine.begin(), l_ine.end(), isspace), l_ine.end());
@@ -39,17 +49,17 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 			co->push_back(new conf_data());
 
 			std::vector<conf_data*>::iterator it= --co->end();
-			f.getline(line, 100, '}');
+			f.getline(line, 1000, '}');
 			std::stringstream content(line);
-			std::string const rules[3] = {"server_names", "port", ""};
+			std::string const rules[6] = {"server_names", "port", "error_page", "root", "host", ""};
 
 			while (!content.eof())
 			{
-				content.getline(line, 100, ':');
+				content.getline(line, 1000, ':');
 				std::string li_ne(line);
 				li_ne.erase(remove_if(li_ne.begin(), li_ne.end(), isspace), li_ne.end());
 				int pos = 0;
-				for (size_t i = 0; i < 3; i++)
+				for (size_t i = 0; i < 6; ++i)
 				{
 					if (li_ne == rules[i]){
 						pos = i + 1;
@@ -59,16 +69,20 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 				switch (pos)
 				{
 					case 1:
-						content.getline(line, 100, ';');
+						content.getline(line, 1000, ';');
 						li_ne = line;
 						if (content.eof())
 								throw std::invalid_argument("no ';' found");
-						(*it)->setServerNames(line);
+						(*it)->addServerNames(line);
 						break;
 					
 					case 2:
+						content.getline(line, 1000, ';');
+						if (doubles.port)
+							break;
+						else
+							doubles.port = true;
 						char *end;
-						content.getline(line, 100, ';');
 						if (content.eof())
 							throw std::invalid_argument("no ';' found");
 						li_ne = line;
@@ -80,6 +94,53 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 						}
 						break;
 					case 3:
+						content.getline(line, 1000, ';');
+						{
+							if (content.eof())
+									throw std::invalid_argument("no ';' found");
+							std::vector<size_t> codes;
+							li_ne = line;
+							char *end;
+							for(size_t j = 0; line[j]; j++)
+							{
+								if (isdigit(line[j])){
+									size_t c = strtol(&line[j], &end, 10);
+									if (!std::isspace(*end)|| std::to_string(c).length() != 3)
+										throw std::invalid_argument("invalid error code");
+									j += 3;
+									codes.push_back(c);
+								}
+								else if(isalpha(line[j])){
+									for (std::vector<size_t>::iterator i = codes.begin(); i != codes.end(); ++i)
+										(*it)->error_pages.insert(std::make_pair(*i, &line[j]));
+									break;
+								}
+							}
+						}
+						break;
+					case 4:
+						content.getline(line, 1000, ';');
+						if (doubles.root)
+							break;
+						else
+							doubles.root = true;
+						li_ne = line;
+						if (content.eof())
+								throw std::invalid_argument("no ';' found");
+						(*it)->setRoot(line);
+						break;
+					case 5:
+						content.getline(line, 1000, ';');
+						if (doubles.host)
+							break;
+						else
+							doubles.host = true;
+						li_ne = line;
+						if (content.eof())
+								throw std::invalid_argument("no ';' found");
+						(*it)->setHost(line);
+						break;
+					case 6:
 						break;
 					default:
 						throw std::invalid_argument("invalid rule");
@@ -90,13 +151,13 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 		}
 		/* CASE "ERROR_PAGE" */
 		else if(l_ine == "error_page"){
-			f.getline(line, 100, '}');
+			f.getline(line, 1000, '}');
 			std::stringstream content(line);
 			std::string const rules[2] = {"codes_to_filepath", ""};
 
 			while (!content.eof())
 			{
-				content.getline(line, 100, ':');
+				content.getline(line, 1000, ':');
 				std::string li_ne(line);
 				li_ne.erase(remove_if(li_ne.begin(), li_ne.end(), isspace), li_ne.end());
 				int pos = 0;
@@ -110,7 +171,7 @@ std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file = 
 				switch (pos)
 				{
 					case 1:
-						content.getline(line, 100, ';');
+						content.getline(line, 1000, ';');
 						{
 							if (content.eof())
 									throw std::invalid_argument("no ';' found");
@@ -155,22 +216,46 @@ int main(int ac, char **av){
 	t_gconf *gconf = new t_gconf;
 	gconf->error_pages = new std::map<size_t, std::string>();
 
-	if (ac == 2)
-		co = readConfFile(gconf, av[1]);
-	else
+	try
+	{
+		if (ac == 2)
+			co = readConfFile(gconf, av[1]);
+		else
+			throw std::invalid_argument("Invalid argument");
+	}
+	catch(const std::exception& e)
+	{
+		Color::Modifier b_red(Color::BG_RED);
+		Color::Modifier f_black(Color::FG_BLACK);
+		Color::Modifier b_def(Color::BG_DEFAULT);
+		Color::Modifier f_def(Color::FG_DEFAULT);
+		std::cerr << b_red << f_black << "\nError: " << e.what() << b_def << '\n';
+		std::cerr << b_red << "Initiating with default settings" << b_def << f_def <<"\n\n" ;
 		co = readConfFile(gconf);
+	}
+	
+	Color::Modifier f_green(Color::FG_GREEN);
+	Color::Modifier f_yellow(Color::FG_YELLOW);
+	Color::Modifier f_def(Color::FG_DEFAULT);
 	std::vector<conf_data*>::iterator it = co->begin();
 	for (size_t i = 0; i < co->size(); i++)
 	{
-		std::cout << "element: " << i + 1 << "\nserver_names: " << (*it)->s_names() << "\nport: " << (*it)->s_port() << std::endl;
+		std::cout << "ELEMENT: " << i + 1 << f_green << "\n\nserver_names: " << (*it)->s_names() << "\nport: " << (*it)->s_port() <<
+		"\nroot_dir: " << (*it)->s_root() << "\nhost: " << (*it)->s_host() << std::endl << std::endl;
+		std::cout << f_def << "elements's error codes\n\n" << f_yellow ;
+		(*it)->printErrorCodes();
+		std::cout << f_def << std::endl << std::endl;
 		++it;
 	}
+	
+	Color::Modifier f_blue(Color::FG_BLUE);
+	std::cout << "DEFAULT ERROR CODES\n\n";
 	for (std::map<size_t, std::string>::iterator i = gconf->error_pages->begin(); i != gconf->error_pages->end(); i++)
 	{
-		std::cout << "code: " << i->first << " path: " << i->second << std::endl;
+		std::cout << f_blue << "default_error_code: " << i->first << " path: " << i->second << f_def << std::endl;
 	}
 	
-	for (std::vector<conf_data*>::iterator ite = co->begin(); ite != co->end(); ite++)
+	for (std::vector<conf_data*>::iterator ite = co->begin(); ite != co->end(); ++ite)
 		delete *ite;
 	
 	delete co;
--- server.hpp
+++ server.hpp
@@ -11,14 +11,39 @@
 #include <arpa/inet.h> // For inet_ntop
 #include <map>
 #include <vector>
-#include <list>
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
 #include <cstring>
 #include <sstream>
+#include <stdlib.h>
 
 #define PORT "4242"
+
+//TEXT COLOR CHANGE
+namespace Color {
+    enum Code {
+		FG_BLACK    = 30,
+        FG_RED      = 31,
+        FG_GREEN    = 32,
+		FG_YELLOW   = 33,
+        FG_BLUE     = 34,
+        FG_DEFAULT  = 39,
+        BG_RED      = 41,
+        BG_GREEN    = 42,
+        BG_BLUE     = 44,
+        BG_DEFAULT  = 49
+    };
+    class Modifier {
+        Code code;
+    public:
+        Modifier(Code pCode) : code(pCode) {}
+        friend std::ostream& operator<<(std::ostream& os, const Modifier& mod) {
+            return os << "\033[" << mod.code << "m";
+        }
+    };
+}
+///////////////////
 
 typedef struct s_gconf{
 	std::map<size_t, std::string> *error_pages;
@@ -28,26 +53,56 @@ class conf_data{
 
 	private:
 		std::string server_names;
+		std::string root;
+		std::string host;
+		std::map<size_t, std::string> error_pages;
 		size_t port;
 	public:
 
-		conf_data() : port(4242){}
+		conf_data() :root("root"), host("localhost"), error_pages(std::map<size_t, std::string>()), port(4242){}
 		conf_data(conf_data const & other){
 			this->server_names = other.server_names;
+			port = other.port;
+			error_pages = other.error_pages;
+			root = other.root;
 		}
 		virtual~conf_data(){}
 
-		void setServerNames(std::string const &s){
-			server_names = s;
+		//SET CONFIGURATION DATA//
+		void addServerNames(std::string const &s){
+			server_names += s;
 		}
 		void setPort(size_t p){
 			port = p;
 		}
+		void setRoot(std::string const &r){
+			root = r;
+		}
+		void setHost(std::string const &s){
+			host = s;
+		}
+
+		//ACCESS CONFIGURATION DATA//
 		std::string const &s_names() const {
 			return server_names;
 		}
+		std::string const &s_root() const {
+			return root;
+		}
 		size_t s_port() const {
 			return this->port;
+		}
+		std::string const &s_host() const {
+			return this->host;
+		}
+		std::map<size_t, std::string> const &s_errorCodes() const {
+			return error_pages;
+		}
+		//////////////////
+
+		void printErrorCodes(){
+			for (std::map<size_t, std::string>::iterator i = error_pages.begin(); i != error_pages.end(); i++)
+				std::cout << "server_block_error_code: " << i->first << " page: " << i->second << std::endl;
 		}
 
 	friend std::vector<conf_data*> *readConfFile(t_gconf *gconf, std::string const &file);
